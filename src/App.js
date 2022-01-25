d241cd928d80791b8b0c4846e546787f33b93cea
shedrachIkenna <shedrach686@gmail.com>
2022-01-25 16:07:14 +0100

    Added responsive form UI in CreateBlog component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/navbar/Navbar'
 import Dashboard from './components/dashboard/Dashboard'
 import BlogDetails from './components/blogs/blogDetails/BlogDetails'
+import CreateBlog from './components/blogs/createBlog/CreateBlog'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
           <Routes>
             <Route exact path='/' element={<Dashboard/>} />
             <Route path='/blog/:id' element={<BlogDetails />} />
+            <Route path='/create' element={<CreateBlog />} />
           </Routes>
         </div>
       </div>
--- src/components/blogs/createBlog/CreateBlog.js
+++ src/components/blogs/createBlog/CreateBlog.js
@@ -1,1 +1,25 @@
 import './createblog.css'
+
+const CreateBlog = () => {
+    return (
+        <div className="create-blog">
+            <div className="create-blog-header">
+                <div className="create-blog-draft-header-box">
+                    <p>Draft</p>
+                    <p>Shedrach ikenna</p>
+                </div>
+                <button className="btn">Publish</button>
+            </div>
+            <div className="create-blog-body">
+                <div className="create-blog-title">
+                    <input type="text" placeholder="Title" id="title"/>
+                </div>
+                <div className="create-blog-content">
+                    <textarea type="text" placeholder="Share you ideas..." id="content"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreateBlog;
