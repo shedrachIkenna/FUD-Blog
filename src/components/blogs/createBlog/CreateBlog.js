e27524254b2c32e1e0a719ebea7d2a081a8e0a10
shedrachIkenna <shedrach686@gmail.com>
2022-01-26 22:51:25 +0100

    Added form disable/enable publish button feature and redux thunk setup

--- src/components/blogs/createBlog/CreateBlog.js
+++ src/components/blogs/createBlog/CreateBlog.js
@@ -1,6 +1,18 @@
 import './createblog.css'
+import { useState } from 'react'
 
 const CreateBlog = () => {
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+
+
+const handleClick = (e) => {
+    e.preventDefault();
+    const blog = { title, content };
+    console.log(blog)
+}
+
+
     return (
         <div className="create-blog">
             <div className="create-blog-header">
@@ -8,14 +20,14 @@ const CreateBlog = () => {
                     <p>Draft</p>
                     <p>Shedrach ikenna</p>
                 </div>
-                <button className="btn">Publish</button>
+                <button className="btn" onClick={handleClick} disabled={!title || !content}>Publish</button>
             </div>
             <div className="create-blog-body">
                 <div className="create-blog-title">
-                    <input type="text" placeholder="Title" id="title"/>
+                    <textarea type="text" placeholder="Title" id="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
                 </div>
                 <div className="create-blog-content">
-                    <textarea type="text" placeholder="Share you ideas..." id="content"/>
+                    <textarea type="text" placeholder="Share you ideas..." id="content" value={content} onChange={(e) => setContent(e.target.value)}/>
                 </div>
             </div>
         </div>
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import RootReducer from './store/reducers/RootReducer'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
-const store = createStore(RootReducer);
+
+const store = createStore(RootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
