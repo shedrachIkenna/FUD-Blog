fb012ddcc6659ba1b51693ca7fb2ceed38022375
shedrachIkenna <shedrach686@gmail.com>
2022-03-06 17:10:28 +0100

    Made grid layout responsive

--- src/App.js
+++ src/App.js
@@ -9,17 +9,17 @@ const App = () => {
   return (
     <Router>
       <div className="App grid gap-3 lg:grid-cols-8 font-inter text-gray-600 max-w-screen-xl border-2 border-red-500 mx-auto">
-          <div className="col-start-1 col-end-3">
+          <div className="lg:col-start-1 lg:col-end-3 md:col-start-1 md:col-end-4">
             <Navbar />
           </div>
-          <div className="col-start-3 col-end-7">
+          <div className="lg:col-start-3 lg:col-end-7 md:col-start-4 md:col-end-9">
             <Routes className="">
               <Route exact path='/' element={<Dashboard/>} />
               <Route path='/blog/:id' element={<BlogDetails />} />
               <Route path='/create' element={<CreateBlog />} />
             </Routes>
           </div>
-          <div className="col-start-7 col-end-9">
+          <div className="lg:col-start-7 lg:col-end-9">
             <Board />
           </div>
       </div>
