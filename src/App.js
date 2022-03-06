884746696e220d8baa5da016022efc159153b9e7
shedrachIkenna <shedrach686@gmail.com>
2022-03-06 16:03:56 +0100

    Added grid to layout

--- src/App.js
+++ src/App.js
@@ -8,22 +8,20 @@ import Board from './components/board/Board'
 const App = () => {
   return (
     <Router>
-      <div className="max-w-screen-xl border-2 border-red-500 mx-auto">
-        <div className="App font-inter text-gray-600">
-          <div className="">
+      <div className="App grid gap-3 lg:grid-cols-8 font-inter text-gray-600 max-w-screen-xl border-2 border-red-500 mx-auto">
+          <div className="col-start-1 col-end-3">
             <Navbar />
           </div>
-          <div className="">
+          <div className="col-start-3 col-end-7">
             <Routes className="">
               <Route exact path='/' element={<Dashboard/>} />
               <Route path='/blog/:id' element={<BlogDetails />} />
               <Route path='/create' element={<CreateBlog />} />
             </Routes>
           </div>
-          <div className="">
+          <div className="col-start-7 col-end-9">
             <Board />
           </div>
-        </div>
       </div>
     </Router>
   );
